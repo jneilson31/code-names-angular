52be3966fddeade94a6e7e0dfe76e71cf65e68da
Jeremy <jeremyneilson31@gmail.com>
2020-10-21 00:07:11 -0600

    progress

--- src/app/board/cards/cards.component.ts
+++ src/app/board/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CardsService, CardValues, CodeNamesCard } from 'src/app/services/cards.service';
 import { GameManagerService } from 'src/app/services/game-manager.service';
@@ -14,6 +14,7 @@ export interface CardVm {
   styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent implements OnInit {
+  @ViewChild('card') card: ElementRef;
   playingCards$ = this.cardsService.cardValuesAndCardDeck$;
   whoseTurn$ = this.gameManager.whoseTurn$;
   cardsVm$: Observable<CardVm>;
@@ -22,18 +23,17 @@ export class CardsComponent implements OnInit {
     private cardsService: CardsService,
     private gameManager: GameManagerService,
     private renderer: Renderer2,
-    private el: ElementRef
   ) { }
 
   ngOnInit(): void {
   }
 
-  public onCardClick(card): void {
-    console.log(card);
-    this.gameManager.checkTurnAndCardValue(card);
+  public onCardClick(event, card): void {
+    console.log(event);
+    this.gameManager.checkTurnAndCardValue(event);
     if (card.value === CardValues.RedAgent) {
-      console.log("got here");
-      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'red');
+      console.log(this.card);
+      this.renderer.addClass(this.card.nativeElement, 'red-card');
     }
   }
 }
